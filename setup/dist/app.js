409519e87e3c643ca323e9a275077cd8d50269ce
Arber Lisaj <arberlisaj1@gmail.com>
2023-04-10 09:32:49 +0200

    TypeScript Compilation

---
+++ setup/dist/app.js
@@ -0,0 +1,21 @@
+"use strict";
+let sales = 2323232323;
+let variable;
+let numbers = [1, 2, 3];
+let user = [10, "arber"];
+let mySize = 2;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+calculateTax(10000, 2022);
+let employee = { id: 1 };
+let a = 100;
+let c = [true, false, true];
+let e = [3];
+let f;
+let g = [];
+//# sourceMappingURL=app.js.map
