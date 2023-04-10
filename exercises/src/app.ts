ad960cb8d5da098c4fca14ddf5208c7898176378
Arber Lisaj <arberlisaj1@gmail.com>
2023-04-10 09:43:07 +0200

    Exercises About TypeScript

---
+++ exercises/src/app.ts
@@ -0,0 +1,8 @@
+// TYPES 
+
+let a = 100 ;  // number
+let c = [true , false , true] //boolean 
+// let d = {age:number} // any 
+let e = [3] // number 
+let f ; // any and can be reasigned any value 
+let g = [] // any 
