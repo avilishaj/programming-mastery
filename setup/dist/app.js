537b5bcd1aa0227a8ec9025d59f7ddf106dd009f
Arber Lisaj <arberlisaj1@gmail.com>
2023-04-10 09:41:46 +0200

    TypeScript Compiler

--- setup/dist/app.js
+++ setup/dist/app.js
@@ -1,10 +1,14 @@
 "use strict";
-let sales = 2323232323;
-let variable;
+let sales = 123456789;
 let numbers = [1, 2, 3];
+numbers.forEach((n) => n.toFixed);
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
 let user = [10, "arber"];
-let mySize = 2;
-console.log(mySize);
 function calculateTax(income, taxYear = 2022) {
     if (taxYear < 2022) {
         return income * 1.2;
@@ -12,10 +16,4 @@ function calculateTax(income, taxYear = 2022) {
     return income * 1.3;
 }
 calculateTax(10000, 2022);
-let employee = { id: 1 };
-let a = 100;
-let c = [true, false, true];
-let e = [3];
-let f;
-let g = [];
 //# sourceMappingURL=app.js.map
