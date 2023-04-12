3052e2f007b25a24991266265de74b1aeb300b18
Arber Lisaj <arberlisaj1@gmail.com>
2023-04-12 21:13:13 +0200

    Added more challenges and solutions

--- exercises/dist/app.js
+++ exercises/dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 let a = 100;
 let c = [true, false, true];
 let e = [3];
@@ -8,8 +9,9 @@ let string = {
     title: "song title",
     releaseYear: 1999,
 };
+(" The Error is : releaseYear is not given when initialized");
 let prices = [100, 200, 300];
-prices[0] = 150;
+prices[0] = 400;
 function myFunction(a, b) {
     return a + b;
 }
@@ -26,5 +28,55 @@ function kgsToLbs(weight) {
         return parseInt(weight) * 2.2;
     }
 }
-console.log(kgsToLbs('10'));
+console.log(kgsToLbs("10"));
 console.log(kgsToLbs(10));
+let laptopWeight = 1;
+laptopWeight = "1kg";
+let textBox = {
+    drag: () => { },
+    resize: () => { },
+};
+let quantity = 50;
+let quantity1 = 100;
+console.log(quantity1);
+function greeting(userName) {
+    if (userName)
+        console.log(`hi ${userName}`);
+    else
+        console.log("null or undefined");
+}
+greeting("arber");
+greeting(null);
+greeting(undefined);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let log = (message) => console.log(message);
+let optionalLog = null;
+optionalLog === null || optionalLog === void 0 ? void 0 : optionalLog("hello world");
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30,
+};
+let phoneInput = document.querySelector("input");
+phoneInput.value;
+let altPhoneInput = document.querySelector("input");
+altPhoneInput.value;
+function render(document) {
+    if (typeof document === "string") {
+        document.toUpperCase();
+    }
+    document.move();
+    document.whateverAndThereIsNoError();
+}
+function reject(message) {
+    throw new Error(message);
+}
+function processEvents() {
+    while (true) {
+    }
+}
+console.log("hello world ");
+const weekday = 'Monday';
--- exercises/src/app.ts
+++ exercises/src/app.ts
@@ -1,11 +1,12 @@
 // TYPES
-
 let a = 100; // number
 let c = [true, false, true]; //boolean
 // let d = {age:number} // any
 let e = [3]; // number
 let f; // any and can be reasigned any value
 let g = []; // any
+
+// ====================================================== //
 
 // Object -- Find the Error
 let string: {
@@ -16,13 +17,16 @@ let string: {
   // We add the releaseYear
   releaseYear: 1999,
 };
-// -- Solution : an Error is occuring because releaseYear is not given when initialized
+(" The Error is : releaseYear is not given when initialized");
+// ====================================================== //
 
 // Array -- Find the Error
 let prices = [100, 200, 300];
 // prices[0] = "$400"; Fixing the Error by returning a number instead
-prices[0] = 150;
+prices[0] = 400;
 // -- Solution : Type string is not asignable to type number
+
+// ====================================================== //
 
 // Functions -- Find the Error
 function myFunction(a: number, b: number): number {
@@ -31,9 +35,11 @@ function myFunction(a: number, b: number): number {
 }
 // -- Solution : myFunction is expected to return a number but nothing is returned
 
-// Type Aliases
-// DRY method stands for Dont Repeat Yourself
+// ====================================================== //
 
+// Type Aliases - Using a type alias we can create a new name (alias) for a type.
+// We often use type aliases to create custom types.
+// DRY method stands for Dont Repeat Yourself
 type Employee = {
   id: number;
   name: string;
@@ -46,14 +52,196 @@ let newEmployee: Employee = {
   age: 19,
 };
 
-// Union Types
+// ====================================================== //
 
+// Union Types -- With union types, we can allow a variable to take one of many types (eg number | string)
 function kgsToLbs(weight: number | string): number {
+  // Narrowing -- Narrow down the union type into a more specific type
   if (typeof weight === "number") {
     return weight * 2.2;
   } else {
     return parseInt(weight) * 2.2;
   }
 }
-console.log(kgsToLbs('10'))
-console.log(kgsToLbs(10))
+console.log(kgsToLbs("10"));
+console.log(kgsToLbs(10));
+
+// --- or ---
+
+let laptopWeight: number | string = 1;
+laptopWeight = "1kg";
+
+// ====================================================== //
+
+// Intersection Type  -- instead of | we now use & , it represents both number and string
+// With intersection types, we can combine multiple types into one (eg Draggable & Resizable)
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizeable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizeable;
+
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+// ====================================================== //
+
+// Literal Types -- limit the values we want to assign a variable
+let quantity: 50 | 100 = 50;
+// we can create custom types using alias instead of hard coding them
+type Quantity = 50 | 100;
+let quantity1: Number = 100;
+type Metric = "cm" | "inch";
+
+console.log(quantity1);
+
+// ====================================================== //
+
+// Nullable Types -- be able to use null and undefined without crashing the app
+
+function greeting(userName: string | null | undefined) {
+  if (userName) console.log(`hi ${userName}`);
+  else console.log("null or undefined");
+}
+
+greeting("arber");
+greeting(null);
+greeting(undefined);
+
+// ====================================================== //
+
+// Optional Chaining
+// Using optional chaining (?.) we can simplify our code and remove the need for null checks.
+type Customer = {
+  // we made the birthday optional
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+  return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+// Optional property access operator
+// if (customer != null) console.log(customer.birthday); make this shorter
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// if(customer !== null && customer !== undefined) => customers?.[0]
+
+// Optional call operator ?.
+let log: any = (message: string) => console.log(message);
+let optionalLog: any = null;
+optionalLog?.("hello world");
+
+// ====================================================== //
+
+// Nullish Coaelsking Operator -- it is called nullish because it includes undefined
+let speed: number | null = null;
+let ride = {
+  // Falsy values in JavaScript (false , null , undefined , '' , 0 )
+  //speed: speed !== null ? speed : 30 is to long so we use :
+  // Nullish Coaelsking Operator
+  speed: speed ?? 30,
+};
+
+// ====================================================== //
+
+// Type Assertion
+// Sometimes we know more about the type of a variable than the TypeScript compiler.
+//  In those situations, we can use the as keyword to specify a different type than the one inferred by the compiler.
+// This is called type assertion.
+let phoneInput = document.querySelector("input") as HTMLInputElement;
+phoneInput.value;
+// --- or ---
+let altPhoneInput = <HTMLInputElement>document.querySelector("input");
+altPhoneInput.value;
+
+// ====================================================== //
+
+// The unknown type -- unknown type > any type because the compiler forces us to do some checkings
+// if you switch from any to unknown the compiler will tell the errors
+
+function render(document: any) {
+  // Narrowing
+  if (typeof document === "string") {
+    document.toUpperCase(); //  ...string methods
+  }
+  document.move();
+  document.whateverAndThereIsNoError();
+}
+
+// Never Type -- Values that never occur
+//The never type represents values that never occur.
+//We often use them to annotate functions that never return or always throw an error.
+
+function reject(message: string): never {
+  throw new Error(message);
+}
+function processEvents(): never {
+  while (true) {
+    // Read a mesage from a queue
+  }
+}
+// processEvents();
+// reject("never");  commented because ente
+// the console.log never runs because the function above never returns
+console.log("hello world ");
+
+// ====================================================== //
+// ====================================================== //
+
+// Exercises
+/*
+•Given the data below, define a type alias for representing users.
+let users = [ 
+   {
+    name:'John Smith',
+    age:30,
+    occupation:'Software engineer'
+  }, 
+    {
+      name:'Kate Müller',
+      age:28
+    }
+];
+*/
+// -- Solution
+type CreateUsers = {
+  name: string;
+  age: number;
+  occupation?: string;
+};
+
+// ====================================================== //
+
+// Birds fly. Fish swim. A Pet can be a Bird or Fish. Use type aliases to represent these
+
+type Bird = {
+  fly: () => void;
+};
+type Fish = {
+  swims: () => void;
+};
+type Pet = Bird | Fish;
+
+// ====================================================== //
+
+// Define a type for representing the days of week. Valid values are “Monday”, “Tuesday”, etc.
+
+type Week =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
+
+  const weekday:Week = 'Monday'
+// ====================================================== //
