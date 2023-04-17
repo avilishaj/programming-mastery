a386a3b69f7d7614297a421135c56d2afa75d2cb
arberlisaj <arberlisaj@DESKTOP-RHDF9AM.domain.name>
2023-04-17 06:07:25 -0400

    rock paper scissors game

--- challenges/dist/app.js
+++ challenges/dist/app.js
@@ -1,3 +1,41 @@
 "use strict";
-console.log('hello machines');
+let userName = "john";
+let choices = ["rock", "paper", "scissors"];
+let userInput = 0;
+let randomNumber = Math.floor(Math.random() * 3);
+let userChoice = choices[userInput];
+let computerChoice = choices[randomNumber];
+if (computerChoice === "rock") {
+    if (userChoice === "rock") {
+        console.log(`${userName}'s choice is the same as the computer's !`);
+    }
+    else if (userChoice === "paper") {
+        console.log(`${userName} won :) `);
+    }
+    else {
+        console.log(`${userName} lost :(`);
+    }
+}
+if (computerChoice === "paper") {
+    if (userChoice === "paper") {
+        console.log(`${userName}'s choice is the same as the computer's !`);
+    }
+    else if (userChoice === "scissors") {
+        console.log(`${userName} won :) `);
+    }
+    else {
+        console.log(`${userName} lost :(`);
+    }
+}
+if (computerChoice === "scissors") {
+    if (userChoice === "scissors") {
+        console.log(`${userName}'s choice is the same as the computer's !`);
+    }
+    else if (userChoice === "rock") {
+        console.log(`${userName} won :) `);
+    }
+    else {
+        console.log(`${userName} lost :(`);
+    }
+}
 //# sourceMappingURL=app.js.map
--- challenges/src/app.ts
+++ challenges/src/app.ts
@@ -1,1 +1,41 @@
-console.log('hello machines')
+// Rock Paper Scissors
+let userName: string = "john";
+let choices = ["rock", "paper", "scissors"];
+let userInput = 0;
+let randomNumber = Math.floor(Math.random() * 3);
+let userChoice = choices[userInput];
+let computerChoice = choices[randomNumber];
+// console.log(choices[userChoice]);
+// console.log(computerChoice);
+
+// building the logics
+
+if (computerChoice === "rock") {
+  if (userChoice === "rock") {
+    console.log(`${userName}'s choice is the same as the computer's !`);
+  } else if (userChoice === "paper") {
+    console.log(`${userName} won :) `);
+  } else {
+    console.log(`${userName} lost :(`);
+  }
+}
+
+if (computerChoice === "paper") {
+  if (userChoice === "paper") {
+    console.log(`${userName}'s choice is the same as the computer's !`);
+  } else if (userChoice === "scissors") {
+    console.log(`${userName} won :) `);
+  } else {
+    console.log(`${userName} lost :(`);
+  }
+}
+
+if (computerChoice === "scissors") {
+  if (userChoice === "scissors") {
+    console.log(`${userName}'s choice is the same as the computer's !`);
+  } else if (userChoice === "rock") {
+    console.log(`${userName} won :) `);
+  } else {
+    console.log(`${userName} lost :(`);
+  }
+}
