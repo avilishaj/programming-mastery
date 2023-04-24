0b65c2072a337fa9d5f303e0c215d845d7785393
arberlisaj <arberlisaj1@gmail.com>
2023-04-24 22:53:05 +0200

    additional challenges for the upcoming days

---
+++ challenges/src/bank-functionality.ts
@@ -0,0 +1,3 @@
+// TODO: create an account using existing annotations from type alias 
+// TODO: set the default balance to 0 
+// TODO: make the atm functionality that the user can deposit withdraw or check his/her balance
